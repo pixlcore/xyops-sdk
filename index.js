f214cdbc8160095a6566fa2d808452ff8f5db806
Joseph Huckaby <jhuckaby@pixlcore.com>
2026-07-12 14:12:40 -0700

    Initial checkin.

---
+++ index.js
@@ -0,0 +1,8 @@
+// xyOps SDK - Main Entrypoint
+// See: https://github.com/pixlcore/xyops-sdk
+// Copyright (c) 2026 PixlCore LLC, BSD 3-Clause License
+
+module.exports = {
+	api: require('./lib/api.js'),
+	job: require('./lib/job.js')
+};
