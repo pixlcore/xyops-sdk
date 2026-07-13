76ba27ec9ec67cecc3b62f0708cf9023632ce7b2
Joseph Huckaby <jhuckaby@pixlcore.com>
2026-07-12 19:30:47 -0700

    Change module.exports to be more ESM-friendly (sigh).

--- index.js
+++ index.js
@@ -2,7 +2,5 @@
 // See: https://github.com/pixlcore/xyops-sdk
 // Copyright (c) 2026 PixlCore LLC, BSD 3-Clause License
 
-module.exports = {
-	api: require('./lib/api.js'),
-	job: require('./lib/job.js')
-};
+exports.api = require('./lib/api.js');
+exports.job = require('./lib/job.js');
